638fb4b42679e1974c72376240d8de611be0955b
rubbymf <114345875+rubbymf@users.noreply.github.com>
2022-10-17 19:00:55 +0700

    Add files via upload

---
+++ ParameterNomor.java
@@ -0,0 +1,18 @@
+
+package PBO;
+
+
+class ParameterNomor {
+    private static void display(int a){
+        System.out.println("Arguments: " + a);
+    }
+
+    private static void display(int a, int b){
+        System.out.println("Arguments: " + a + " and " + b);
+    }
+
+    public static void main(String[] args) {
+        display(1);
+        display(1, 4);
+    }
+}
